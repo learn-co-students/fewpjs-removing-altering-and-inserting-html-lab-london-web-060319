cac290882708019095c67f0e45bbbff046db534d
Le H Ngo <seiji@o2.pl>
2019-07-12 21:22:37 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const element = document.querySelector("main#main")
+element.remove()
+
+const newH1 = document.createElement("h1")
+newH1.setAttribute("id", "victory")
+const newContent = document.createTextNode("YOUR-NAME is the champion")
+
+newH1.appendChild(newContent)
+document.body.appendChild(newH1)
+
+
+const newHeader = document.querySelector("h1#victory")
